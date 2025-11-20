a9eb7a433cfcfcd69eb6465ccae07616510797f1
SebaConde <sebaconde7@hotmail.es>
2025-11-20 11:54:10 -0300

    Prueba para prod

--- src/components/featured-products.tsx
+++ src/components/featured-products.tsx
@@ -22,7 +22,7 @@ const FeaturedProducts = () => {
       <h3 className="px-6 text-3xl sm:pb-8">Productos destacados</h3>
 
       <Carousel>
-        <CarouselContent className="-ml-2 md:-ml-4">
+        <CarouselContent className="ml-2 md:-ml-4">
           {loading && <SkeletonSchema grid={3} />}
           {result != null &&
             result.map((product: ProductType) => {
@@ -32,8 +32,8 @@ const FeaturedProducts = () => {
                 <CarouselItem key={id} className="md:basis-1/2 lg:basis-1/3 group">
                   <div className="p-1 ">
                     <Card className=" py-4 border border-gray-200 shadow-none">
-                      <CardContent className="relative flex items-center justify-center px-6 py-2">
-                        <img src={`${process.env.NEXT_PUBLIC_BACKEND_URL}${images[0].url}`} alt="Image featured" height={400} width={400}/>
+                      <CardContent className="relative flex items-center justify-center px-6 py-2 h-80">
+                        <img src={`${process.env.NEXT_PUBLIC_BACKEND_URL}${images[0].url}`} alt="Image featured" className="w-full h-full object-contain"/>
                         <div className="absolute w-full px-6 bottom-5 opacity-100 sm:opacity-0 sm:group-hover:opacity-100 transition duration-200">
                           <div className="flex justify-center gap-x-6">
                             <IconButton onclick={() => router.push(`product/${slug}`)} icon={<Expand size={20} />} className="text-gray-600" />
